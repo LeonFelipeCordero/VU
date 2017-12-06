d45a4234e0dd241394b37b35b7afcdee4f5d64f8
Leon Felipe Cordero <zolopretroriano@gmail.com>
2017-12-05 19:37:33 -0700

    remove elasticsearch stuff

--- SafeCityAPI/src/main/java/com/ph/service/IncidentServiceImpl.kt
+++ SafeCityAPI/src/main/java/com/ph/service/IncidentServiceImpl.kt
@@ -3,10 +3,8 @@ package com.ph.service
 import com.ph.dao.IncidentDao
 import com.ph.form.IncidentForm
 import com.ph.model.Incident
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint
 import org.springframework.stereotype.Service
-
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
