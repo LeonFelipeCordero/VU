759190731121d76644680ed9042ac4a477c804d0
LeonFelipeCordero <zolopretroriano@gmail.com>
2018-11-18 16:38:51 +0100

    more config

--- SafeCity/src/main/kotlin/com/ph/SafeCityApplication.kt
+++ SafeCity/src/main/kotlin/com/ph/SafeCityApplication.kt
@@ -2,10 +2,8 @@ package com.ph
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
-@EnableAsync(proxyTargetClass = true)
 open class SafeCityApplication
 
 fun main(args: Array<String>) {
